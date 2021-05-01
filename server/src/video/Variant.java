6b732a78f685cca7640c00b340cca7521af9989a
NickSiak <nikos.siakas@gmail.com>
2021-05-01 21:31:43 +0300

    server: add equals method to variant

--- server/src/video/Variant.java
+++ server/src/video/Variant.java
@@ -16,4 +16,23 @@ public class Variant {
   public Resolution getResolution() {
     return this.resolution;
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+
+    if (obj.getClass() != this.getClass()) {
+      return false;
+    }
+
+    final Variant other = (Variant) obj;
+    
+    if (this.format != other.format) {
+      return false;
+    }
+
+    return this.resolution == other.resolution;
+  }
 }
