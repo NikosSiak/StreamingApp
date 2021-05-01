03fb4ef394885ab5595392c895a766f88d4763a0
NickSiak <nikos.siakas@gmail.com>
2021-05-01 21:42:00 +0300

    server: video variants implent getMissingVariants

--- server/src/video/VideoVariants.java
+++ server/src/video/VideoVariants.java
@@ -5,18 +5,38 @@ import java.util.ArrayList;
 public class VideoVariants {
   private String videoName;
   private ArrayList<Variant> variants;
+  private Resolution maxResolution;
 
   public VideoVariants(String videoName) {
     this.videoName = videoName;
     this.variants = new ArrayList<>();
+    this.maxResolution = Resolution.RES_240;
   }
   
   public void addVariant(Format fileFormat, Resolution resolution) {
     this.variants.add(new Variant(fileFormat, resolution));
+    if (maxResolution.isLessThan(resolution)) {
+      maxResolution = resolution;
+    }
   }
 
   public Variant[] getMissingVariants() {
-    return new Variant[0];
+    ArrayList<Variant> missingVariants = new ArrayList<>();
+    
+    for (Format format : Format.values()) {
+      for (Resolution resolution : Resolution.values()) {
+        if (resolution.isGreaterThan(this.maxResolution)) {
+          break;
+        }
+        
+        Variant variant = new Variant(format, resolution);
+        if (!variants.contains(variant)) {
+          missingVariants.add(variant);
+        }
+      }
+    }
+
+    return missingVariants.toArray(new Variant[missingVariants.size()]);
   }
 
   public String getVideoName() {
