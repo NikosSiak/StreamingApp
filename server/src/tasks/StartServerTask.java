f0c899b45818382320a142ce859d650d1b00d941
NickSiak <nikos.siakas@gmail.com>
2021-05-02 20:18:40 +0300

    server: StartServerTask start listening for connection

--- server/src/tasks/StartServerTask.java
+++ server/src/tasks/StartServerTask.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import net.bramp.ffmpeg.FFmpegExecutor;
 
@@ -23,6 +26,30 @@ public class StartServerTask implements Runnable {
   public void run() {
     GenerateVideosTask generateVideosTask = new GenerateVideosTask(this.videosFolder, this.ffmpegExecutor);
     generateVideosTask.run();
+
+    ServerSocket serverSocket;
+    try  {
+      serverSocket = new ServerSocket(1312);
+    } catch (IOException e) {
+      LOGGER.error("Failed to start server: {}", e.getMessage());
+      return;
+    }
+    
+    Thread currentThread = Thread.currentThread();
+    while (!currentThread.isInterrupted()) {
+      try {
+        Socket clientSocket = serverSocket.accept();
+        Thread handleClientThread = new Thread(new HandleClientTask(clientSocket));
+        handleClientThread.start();
+      } catch (IOException e) {
+        LOGGER.error(e.getMessage());
+      }
+    }
+
+    try {
+      serverSocket.close();
+    } catch (IOException e) {
+      LOGGER.error("Failed to close server: {}", e.getMessage());
+    }
   }
-  
 }
